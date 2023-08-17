9c00bdfcc2c7ef626e633db9581ebc5cfb99799a
Dhruti313 <124250238+Dhruti313@users.noreply.github.com>
2023-08-17 19:07:31 +0530

    'get employee by id'

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ResourcesNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,9 +19,20 @@ public class EmployeeController {
      public List<Employee> getAllEmployee(){
          return employeeRepository.findAll();
      }
+
+     //build create employee
      @PostMapping
     public Employee createEmployee( @RequestBody Employee employee){
         return employeeRepository.save(employee);
     }
+//build getEmployee by id
+@GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById( @PathVariable long id){
+            Employee employee = employeeRepository.findById(id).
+                    orElseThrow(()->new ResourcesNotFoundException("Employee not exsist with id : +id"));
+            return ResponseEntity.ok(employee);
+
+    }
+
 
 }
